8221b5526a49cca5ab54e21d7c37de5992c99cf8
佐藤慎也 <sato.shinya@mdpr.jp>
2022-03-11 00:19:08 +0900

    feat: first up

---
+++ lambda.go
@@ -0,0 +1,29 @@
+package main
+
+type inputData struct {
+	ID *int `json:"id"`
+}
+
+func (i *inputData) Validate() error {
+	return nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, in *inputData) error {
+	deadline, _ := ctx.Deadline()
+	childCtx, cancel := context.WithDeadline(ctx, deadline.Add(-100*time.Millisecond))
+	defer cancel()
+
+	if err := in.Validate(); err != nil {
+		return fmt.Errorf("input validate failure:  %+v", err)
+	}
+
+	return run(childCtx)
+}
+
+func run(ctx context.Context) error {
+	return nil
+}
